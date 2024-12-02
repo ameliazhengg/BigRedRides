0b6db07e5dcf4ca99943e37ed9361776feb3b6ed
Layla Liu <layla.xin.liu@gmail.com>
2024-12-02 16:22:29 -0500

    Initial Commit

---
+++ Ride Sharing/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Ride Sharing
+//
+//  Created by Layla Liu on 12/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Ride Sharing/Ride_SharingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ride_SharingApp.swift
+//  Ride Sharing
+//
+//  Created by Layla Liu on 12/2/24.
+//
+
+import SwiftUI
+
+@main
+struct Ride_SharingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
